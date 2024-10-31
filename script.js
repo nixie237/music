29bafc9c334c98af735557fcaae17eb609bafcc0
nixie237 <elizabethwanjiku612@gmail.com>
2024-10-31 08:33:38 +0300

    this is my new changes

--- script.js
+++ script.js
@@ -1,25 +1,73 @@
 let play = document.querySelector("#play");
 let playing = document.querySelector("#playing");
- play.addEventListener("click", () => {
- // Hide this button
- play.style = "display: none";
- playing.style = " ";
+play.addEventListener("click", () => {
+  // Hide this button
+  play.style = "display: none";
+  playing.style = " ";
 
- Tone.start();
- let synth = new Tone.Synth().toDestination();
- new Tone.Sequence( (time, note) => {
- synth.triggerAttackRelease(note, "16n", time);
- },  ["C4", null, "B3", "C4", "G3", "A3", null, "B3"], "8n")
- .start("0:0:0")
- .stop("4:0:0");
-
-    Tone.Transport.start();
-
- });
- synth.triggerAttackRelease("G4", "16n", "0:0:0");
- synth.triggerAttackRelease("C4", "16n", "0:1:0");
- synth.triggerAttackRelease("C4", "16n", "0:2:0");
- synth.triggerAttackRelease("C4", "16n", "0:3:0");
- synth.triggerAttackRelease("G4", "16n", "1:0:0");
- synth.triggerAttackRelease("C4", "16n", "1:1:0");
- 
+  Tone.start();
+  let reverb = new Tone.Reverb({
+    decay: 1,
+    wet: 0.3,
+  }).toDestination();
+  let hiHatFilter = new Tone.Filter(15000, "bandpass").connect(reverb);
+  let hiHat = new Tone.NoiseSynth({
+    envelope: {
+      attack: 0.001,
+      decay: 0.1,
+      sustain: 0,
+      release: 0,
+    },
+    volume: -6,
+  }).connect(hiHatFilter);
+  new Tone.Loop((time) => {
+    hiHat.triggerAttackRelease("16n", time);
+  }, "8n")
+    .start("0:0:0")
+    .stop("4:0:0");
+  class Snare {
+    constructor() {
+      this.noiseFilter = new Tone.Filter(5000, "bandpass").connect(reverb);
+      this.noiseSynth = new Tone.NoiseSynth({
+        envelope: {
+          attack: 0.001,
+          decay: 0.1,
+          sustain: 0,
+          release: 0,
+        },
+        volume: -12,
+      }).connect(this.noiseFilter);
+      this.synth = new Tone.Synth({
+        envelope: {
+          attack: 0.0001,
+          decay: 0.1,
+          sustain: 0,
+          release: 0,
+        },
+        oscillator: { type: "sine" },
+        volume: -12,
+      }).connect(reverb);
+    }
+    triggerAttackRelease(duration, when) {
+      this.noiseSynth.triggerAttackRelease(duration, when);
+      this.synth.triggerAttackRelease("G3", duration, when);
+    }
+  }
+  let snare = new Snare();
+  new Tone.Loop((time) => {
+    snare.triggerAttackRelease("16n", time);
+  }, "2n")
+    .start("0:1:0")
+    .stop("4:0:0");
+  let kick = new Tone.MembraneSynth({
+    pitchDecay: 0.02,
+    octaves: 6,
+    volume: -9,
+  }).connect(reverb);
+  new Tone.Loop((time) => {
+    kick.triggerAttackRelease(50, "16n", time);
+  }, "2n")
+    .start("0:0:0")
+    .stop("4:0:0");
+  Tone.Transport.start();
+});
